ae17b12b9059f97bf74d4282117e2471a8f1bc75
sandromiano <sandro.miano@yale.edu>
2023-03-09 16:21:20 -0800

    nothing

--- tests/test_Nlosc.py
+++
@@ -1,6 +0,0 @@
-from ninatool.internal.structures import Nlosc
-from ninatool.circuits.base_circuits import snail
-from ninatool.gui.mainwindow import ninaGUI
-
-spa = Nlosc(nlind = snail(), name = 'SPA')
-ninaGUI(spa)
