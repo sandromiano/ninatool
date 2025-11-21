ca7aeedb71b692ca5ed47afeaa6f12d20b4e1ecf
sandromiano <sandro.miano@yale.edu>
2025-11-20 22:46:46 -0500

    Update structures.py

--- ninatool/internal/structures.py
+++ ninatool/internal/structures.py
@@ -581,7 +581,7 @@ class Nlosc:
         self.__name = name
         self.__kind = 'nlosc'
         self.__nlind = nlind
-        self.__C = C(name = name + '.C0')
+        self.__C = C(1, name = name + '.C0')
 
     @property
     def name(self):
